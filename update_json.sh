a2d52fbfe2666e9aae0b72c4294b59326afeda89
Divya Manian <divya.manian@gmail.com>
2011-12-26 03:32:20 +0000

    Adding twitter update script

---
+++ update_json.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Updates the twitter.json file
+
+set -e
+
+BASE=$(dirname $0)
+
+cd $BASE
+
+git pull --rebase > /dev/null 2>&1 
+
+node lib/node/ebbflow.js 
+
+STATUS=$(git status --porcelain)
+
+if [ -n "$STATUS" ]
+then
+  git commit -a -m "Updating twitter.json" > /dev/null 2>&1
+  git push origin gh-pages
+fi
+
+
