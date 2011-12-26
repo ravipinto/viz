c5166c7144a3d09c91ca2731a69c32058b58b27b
Divya Manian <divya.manian@gmail.com>
2011-12-26 03:39:56 +0000

    Tweaking update script

--- update_json.sh
+++ update_json.sh
@@ -17,7 +17,7 @@ STATUS=$(git status --porcelain)
 if [ -n "$STATUS" ]
 then
   git commit -a -m "Updating twitter.json" > /dev/null 2>&1
-  git push origin gh-pages
+  git push origin gh-pages > /dev/null 2>&1
 fi
 
 
